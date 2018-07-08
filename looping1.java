c02ba0fa22ac50e777e22faa0f71c536974a375f
ibrahimindo <40917417+ibrahimindo@users.noreply.github.com>
2018-07-08 20:28:02 +0700

    Add files via upload

---
+++ looping1.java
@@ -0,0 +1,17 @@
+public class looping1
+{
+	public static void main(String[] args)
+	{
+		int a;
+		int b;
+		
+		for (a=1; a<=5; a++)
+		{
+			for (b=1; b<=a; b++)
+			{
+				System.out.print(b-" ");
+			}
+			System.out.println();
+		}
+	}
+}
